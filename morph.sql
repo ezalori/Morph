a81d521a80262834a551f94bc6bc0ea013e3a870
Ji ZHANG <zhangji87@gmail.com>
2022-04-10 15:11:14 +0800

    use spring data jdbc

--- morph.sql
+++ morph.sql
@@ -6,7 +6,18 @@ create table morph.extract_table (
   ,target_instance int not null
   ,target_database varchar(255) not null
   ,target_table varchar(255) not null
-  ,column_list text not null
+  ,column_list varchar(2000) not null default ''
+  ,created_at datetime not null
+  ,updated_at timestamp not null default current_timestamp on update current_timestamp
+);
+
+create table morph.database_instance (
+  id int primary key auto_increment
+  ,name varchar(255) not null
+  ,host varchar(255) not null
+  ,port int not null
+  ,username varchar(255) not null
+  ,password varchar(255) not null
   ,created_at datetime not null
   ,updated_at timestamp not null default current_timestamp on update current_timestamp
 );
