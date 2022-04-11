f23e22cc727a56f5dc3124428752d068731f0b60
Ji ZHANG <zhangji87@gmail.com>
2022-04-11 14:43:21 +0800

    delete table

--- web/src/main/java/org/ezalori/morph/web/controller/TableController.java
+++ web/src/main/java/org/ezalori/morph/web/controller/TableController.java
@@ -2,7 +2,6 @@ package org.ezalori.morph.web.controller;
 
 import java.util.Date;
 import java.util.Map;
-
 import javax.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
@@ -13,9 +12,7 @@ import org.ezalori.morph.web.service.ExtractTableService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Sort;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +33,7 @@ public class TableController {
 
   @GetMapping("/get")
   public ApiResponse get(@RequestParam("id") Integer id) {
-    var table = tableRepo.findById(id).orElseThrow(() -> new ApiException("id not found"));
+    var table = tableRepo.findById(id).orElseThrow(() -> new ApiException("Table ID not found."));
     var columns = tableService.getColumns(
         table.getSourceInstance(), table.getSourceDatabase(), table.getSourceTable());
     return new ApiResponse(Map.of("table", table, "columns", columns));
@@ -50,7 +47,7 @@ public class TableController {
 
     ExtractTable table;
     if (tableForm.getId() != null) {
-      table = tableRepo.findById(tableForm.getId()).orElseThrow(() -> new ApiException("id not found"));
+      table = tableRepo.findById(tableForm.getId()).orElseThrow(() -> new ApiException("Table ID not found."));
     } else {
       table = new ExtractTable();
       table.setCreatedAt(new Date());
@@ -58,6 +55,15 @@ public class TableController {
     BeanUtils.copyProperties(tableForm, table);
     tableRepo.save(table);
     return new ApiResponse(Map.of("id", table.getId()));
+  }
+
+  @PostMapping("/delete")
+  public ApiResponse delete(@RequestParam("id") Integer id) {
+    if (!tableRepo.existsById(id)) {
+      throw new ApiException("Table ID not found.");
+    }
+    tableRepo.deleteById(id);
+    return new ApiResponse(Map.of("message", "ok"));
   }
 
   @GetMapping("/columns")
