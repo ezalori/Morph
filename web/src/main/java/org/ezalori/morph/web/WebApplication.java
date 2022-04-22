89fa04f221634b94d09bea137537fc1c7c67ab8f
Ji ZHANG <zhangji87@gmail.com>
2022-04-22 17:11:49 +0800

    cleanup imports

--- web/src/main/java/org/ezalori/morph/web/WebApplication.java
+++ web/src/main/java/org/ezalori/morph/web/WebApplication.java
@@ -1,28 +1,11 @@
 package org.ezalori.morph.web;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 /**
--- web/src/main/java/org/ezalori/morph/web/controller/UserController.java
+++ web/src/main/java/org/ezalori/morph/web/controller/UserController.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.ezalori.morph.web.model.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
-  @PostMapping("/current")
-  public Map<String, Object> login(@AuthenticationPrincipal User user) {
+  @GetMapping("/current")
+  public Map<String, Object> current(@AuthenticationPrincipal User user) {
     return Map.of("id", user.getId(), "username", user.getUsername());
   }
 }
