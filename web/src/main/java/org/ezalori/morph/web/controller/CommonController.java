9702a60281bbae4087906fc90b086337d389454b
Ji ZHANG <zhangji87@gmail.com>
2022-05-10 09:12:42 +0800

    Add springdoc

--- web/src/main/java/org/ezalori/morph/web/controller/CommonController.java
+++ web/src/main/java/org/ezalori/morph/web/controller/CommonController.java
@@ -1,7 +1,7 @@
 package org.ezalori.morph.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class CommonController {
-  @RequestMapping(value = "/ping")
+  @GetMapping("/ping")
   @ResponseBody
   public String ping() {
     return "pong";
